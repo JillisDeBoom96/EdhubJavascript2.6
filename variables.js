11a47054b67966f471a382322be44b50faa1c08e
JillisDeBoom <Jillis123@hotmail.nl>
2021-03-30 14:25:12 +0200

    repaired task

--- variables.js
+++ variables.js
@@ -3,41 +3,42 @@
 // ==========================================
 
 const start = "Let's go!";
+console.log(start)
 
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-let name = "Jillis";
+const name = "Jillis";
 console.log(name);
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-let color = "Blue eye";
+let color = "Blue eyes";
 console.log(color);
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-const age = "24";
+let age = "24";
 console.log(age);
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
-const job = "Student";
+let job = "Student";
 console.log(job);
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
-let shoeSize = "45"
+const shoeSize = "45"
 console.log(shoeSize);
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
-const partner = "I don't know"
+let partner = "I don't know"
 console.log(partner);
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
-const children = "none"
+let children = "none"
 console.log(children);
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
